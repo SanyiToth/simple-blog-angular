5d4399b3406591bdd316c9867460b2fe2f25b4e2
Sanyi Tóth <sandor.toth@hotmail.com>
2021-04-20 20:21:16 +0200

    postComments() method tested and works

--- src/app/features/comments/comment-form-container/comment-form-container.component.ts
+++ src/app/features/comments/comment-form-container/comment-form-container.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {NewComment} from '../new-comment.interface';
+import {CommentsService} from '../comments.service';
 
 @Component({
   selector: 'app-comment-form-container',
@@ -7,15 +8,24 @@ import {NewComment} from '../new-comment.interface';
   styleUrls: ['./comment-form-container.component.css']
 })
 export class CommentFormContainerComponent implements OnInit {
+  @Input() postId: number;
   comment: NewComment;
+  errorMessage;
 
 
-  constructor() {
+  constructor(private commentService: CommentsService) {
   }
 
   setComment(newComment: NewComment): void {
     this.comment = newComment;
     console.log('this.comment', this.comment);
+    this.commentService.postComments(this.comment)
+      .subscribe(response => {
+        console.log('postComments()', response);
+      }, error => {
+        console.log('error', error);
+        this.errorMessage = error;
+      });
   }
 
 
