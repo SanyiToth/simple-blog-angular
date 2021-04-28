e8f82aa1d10e99902a503b9a9d78616c23dbcfc4
Sanyi Tóth <sandor.toth@hotmail.com>
2021-04-28 21:11:34 +0200

    core module imported in other moduls

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './components/header/header.component';
+import {MatCardModule} from '@angular/material/card';
+import {PageComponent} from './components/page/page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatCardModule
+  ],
+  exports: [HeaderComponent]
 })
-export class CoreModule { }
+export class CoreModule {
+}
