86b6af6a0d07c6f9be1d2c695ead6f8770206473
Sanyi Tóth <sandor.toth@hotmail.com>
2021-04-28 19:51:56 +0200

    posts.component moved to posts-container directory

--- src/app/features/posts/post-routing.module.ts
+++ src/app/features/posts/post-routing.module.ts
@@ -1,7 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PostDetailsComponent} from './post-details/post-details.component';
-import {PostsComponent} from './posts.component';
+import {PostsComponent} from './posts-container/posts.component';
+
 
 
 const routes: Routes = [
