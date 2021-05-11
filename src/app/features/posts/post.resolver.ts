d4dc22eeb08444df507ee7a61d5719c428dcc39a
Sanyi Tóth <sandor.toth@hotmail.com>
2021-05-11 14:04:57 +0200

    Angular SimpleBlog

--- src/app/features/posts/post.resolver.ts
+++ src/app/features/posts/post.resolver.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   Router, Resolve,
   RouterStateSnapshot,
   ActivatedRouteSnapshot
 } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import {Observable, of} from 'rxjs';
+import {Post} from './post.interface';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PostResolver implements Resolve<boolean> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+export class PostResolver implements Resolve<Post[]> {
+
+  constructor(private http: HttpClient) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Post[]> {
+    return this.http.get<Post[]>('http://localhost:3000/posts');
   }
 }
