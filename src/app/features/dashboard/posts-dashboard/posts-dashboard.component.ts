e959e44c0e2dd6b403324d622a4f45cf70da7e00
Sanyi Tóth <sandor.toth@hotmail.com>
2021-05-21 01:21:42 +0200

    posts listed in post dashboard component

--- src/app/features/dashboard/posts-dashboard/posts-dashboard.component.ts
+++ src/app/features/dashboard/posts-dashboard/posts-dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from '../../posts/posts.service';
+import {Post} from '../../posts/post.interface';
 
 @Component({
   selector: 'app-posts-dashboard',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-dashboard.component.css']
 })
 export class PostsDashboardComponent implements OnInit {
+  posts: Post [];
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private postsService: PostsService) {
+  }
 
   ngOnInit(): void {
+    this.postsService.getPosts().subscribe(posts => {
+      this.posts = posts;
+    }, errorMsg => {
+      this.errorMessage = errorMsg;
+    });
   }
 
 }
